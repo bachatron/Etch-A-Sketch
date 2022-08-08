795399ed3e896bb0f29baa3365c816543c237431
bachatron <idz420@gmail.com>
2022-08-08 20:25:40 -0300

    Grid code done, grid size can change

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 const container = document.getElementById("container");
+const eraser = document.getElementById("eraser");
+const slider = document.getElementById("gridSize");
+const output = document.getElementById("size");
+const rgbButton = document.getElementById("rgbMode")
+
+rgbButton.addEventListener('click', rgbMode);
+
+eraser.addEventListener('click', eraseGrid)
 
 function addManyDivs (elementTarget, numberOfDivs) {
-    ;
-    for (let i = 0; i < numberOfDivs; i++){
+    //This function creates a new grid of the desire size
+    for (let i = 0; i < numberOfDivs*numberOfDivs; i++){
         const newDiv = document.createElement("div");
-        newDiv.addEventListener("mouseover", (event) => {newDiv.classList.toggle("hover")});
+        newDiv.style.width = 600 / numberOfDivs + "px";
+        newDiv.style.height = 600 / numberOfDivs + "px";
+        newDiv.addEventListener("mousedown", (event) => {newDiv.classList.toggle("hover")});
         
         elementTarget.appendChild(newDiv);
-    }
-    
-
-   
+    }   
 
 }
+function eraseGrid() {
+    //This function makes every square of the grid white
+    for (const element of container.children){
+        element.classList.remove('hover');
+    }
+}
+function deleteGrid() {
+    //This function removes the grid so we can add a new one
+    while (container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+}
 
-addManyDivs(container, 256);
+function rgbMode() {
+    //This function makes every square of the grid white
+    for (const element of container.children){
+        console.log(element)
+        removeEventListener("mousedown", (event) => {element.classList.toggle("hover")});
+    }
+}
+
+addManyDivs(container, 8);
+
+
+output.innerHTML = slider.value*8 + "x" + slider.value*8;
+
+slider.oninput = function() {
+  output.innerHTML = this.value*8+ "x" + slider.value*8;
+  deleteGrid();
+  addManyDivs(container, this.value*8);
+}
+
+
